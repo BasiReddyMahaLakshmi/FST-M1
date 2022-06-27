ef1cf6c70f0540fafd5b3771ac6884985f8578a5
BasiReddyMahaLakshmi <106366184+BasiReddyMahaLakshmi@users.noreply.github.com>
2022-06-27 20:55:23 +0530

    Add files via upload

---
+++ SQL/Activities/Activity7.sql.sql
@@ -0,0 +1,13 @@
+REM   Script: Activity7
+REM   Activity7
+
+select SUM(purchase_amount) from orders;
+
+select AVG(purchase_amount) from orders;
+
+select MAX(purchase_amount) from orders;
+
+select MIN(purchase_amount) from orders;
+
+select COUNT(salesman_id) from orders;
+
